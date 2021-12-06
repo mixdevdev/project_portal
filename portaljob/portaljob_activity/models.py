d36a6fc020c124cabf34d2272ddbc1653755d0fe
ranoa <ranoaritsiky@gmail.com>
2021-12-06 22:44:51 +0300

    create new model

--- portaljob/portaljob_activity/models.py
+++ portaljob/portaljob_activity/models.py
@@ -4,9 +4,12 @@ from registration.models import User
 
 # Create your models here.
 
+class Administration(models.Model):
+    coverage_photo=models.ImageField(upload_to='portaljob_activity')
+
 class Recruitment_info(models.Model):
     name=models.CharField(max_length=200,default='')
-    pub_date=models.DateField('date de publication')
+    pub_date=models.DateField('date de publication',default='')
     user_id=models.ForeignKey(User, on_delete=models.CASCADE)
     end_recruitment_date=models.DateField('Fin de recruitement',default='')
     description=models.TextField('Description on the job',default='')
